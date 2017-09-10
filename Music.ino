b9226c072f34e7539d4b856c37ff94a4139d1aaf
严老寒 <yanchenbaigao@163.com>
2017-09-10 17:05:45 +0800

    测试成功，谱子写了一半

--- Music.ino
+++ Music.ino
@@ -1,15 +1,15 @@
 
-#define T 500
+#define T 450
 
-int src[11] = {0, 262, 294, 330, 349, 392, 440, 494, 523, 587, 659};
-//         None, 1 , 2 , 3 , 4 , 5 , 6 , 7 , 1+, 2+, 3+
+int src[12] = {0, 262, 294, 330, 349, 392, 440, 494, 523, 587, 659,698};
+//           None, 1 ,  2 ,  3 ,  4 ,  5 ,  6 ,  7 ,  1+,  2+,  3+, 4+
 
 void push(int m, float t) {
   if(m == 0){
     delay(T*t);
     return;
   }
-  c = 1.0 / src[m] * 1000000 - 100;
+  float c = 1.0 / src[m] * 1000000 - 100;
   for (int i = 0; i <= 1.0 * T * t; i += 1.0 * (c + 100) / 1000) {
     digitalWrite(2, HIGH);
     delayMicroseconds(100);
@@ -34,9 +34,146 @@ void loop() {
   push(0,1);
   push(3,0.5);
   push(5,0.5);
-  push(6,1.5);
+  
+  push(6,1);
   push(0,0.5);
   push(5,0.5);
-  push(6,1.5);
+  push(6,1);
+  
   push(0,0.5);
+  push(5,0.5);
+  push(6,0.5);
+  push(8,0.3);
+  push(5,0.5);
+  push(6,0.5);
+  push(3,1);
+  push(0,1);
+
+  push(3,0.5);
+  push(5,0.5);
+  push(6,1);
+  push(0,0.5);
+  push(5,0.5);
+  push(6,1);
+
+  push(0,0.5);
+  push(5,0.5);
+  push(6,0.5);
+  push(10,0.3);
+  push(8,0.3);
+  push(9,0.3);
+  push(6,1);
+  
+  push(3,0.5);
+  push(5,0.5);
+  push(6,1);
+  push(0,0.5);
+  push(5,0.5);
+  push(6,1);
+  //
+  push(0,0.5);
+  push(5,0.5);
+  push(6,0.5);
+  push(8,0.3);
+  push(5,0.5);
+  push(6,0.5);
+  push(3,0.5);
+  push(5,0.5);
+  push(1,0.6);
+  push(2,0.6);
+
+  push(3,0.5);
+  push(8,0.3);
+  push(6,0.5);
+  push(10,0.3);
+
+  push(9,0.25);
+  push(10,0.15);
+  push(9,0.15);
+  push(8,0.2);
+  push(9,0.2);
+  push(6,0.3);
+
+  //
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.2);
+  push(8,0.2);
+  push(9,0.2);
+  push(10,0.2);
+
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.5);
+  push(3,0.2);
+  push(0,0.3);
+  push(3,0.7);
+  push(6,0.2);
+
+  push(0,0.5);
+
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.2);
+  push(8,0.2);
+  push(9,0.2);
+  push(10,0.2);
+
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.5);
+  push(11,0.8);
+  push(10,0.5);
+
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.2);
+  push(8,0.2);
+  push(9,0.2);
+  push(10,0.2);
+
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.5);
+  push(3,0.2);
+  push(0,0.3);
+  push(3,0.7);
+  push(6,0.2);
+
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.5);
+  push(0,0.5);
+  push(6,0.2);
+  push(8,0.2);
+  push(9,0.2);
+  push(10,0.2);
+
+  push(6,0.5);
+  push(0,0.3);
+  push(5,0.3);
+  push(0,0.3);
+  push(5,0.5);
+  push(6,0.2);
+  push(0,0.3);
+  push(6,0.5);
+  
 }
